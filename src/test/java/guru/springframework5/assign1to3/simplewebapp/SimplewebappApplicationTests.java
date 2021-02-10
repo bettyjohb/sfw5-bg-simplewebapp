bd20679a2504eb9861a9ec2a09b01475cd68b6a6
bettyjohb <bjhb.goomail@gmail.com>
2021-02-10 12:45:20 -0800

    First commit - Unmodified simplewebapp project from Initializr

---
+++ src/test/java/guru/springframework5/assign1to3/simplewebapp/SimplewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework5.assign1to3.simplewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
